3bafd9c1007e4e924bb6f5c247bbe10ad23eb861
Madison May <madison.may@students.olin.edu>
2013-06-25 08:22:07 +0200

    Fixed assign to allow aliases that map to phrases instead of single word commands

--- routes/code.js
+++ routes/code.js
@@ -2,3 +2,7 @@
 exports.automata = function(req, res){
   res.render('automata', { title: 'Cellular Automata' })
 };
+
+exports.automata = function(req, res){
+  res.render('debug', { title: 'Exterminator.js' })
+};
