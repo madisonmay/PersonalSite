d8a5a71270dc9ecdefcbbb0d77de13437bdd6d16
Madison May <madison.may@students.olin.edu>
2013-06-29 17:51:59 +0200

    Assorted commits

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ app.configure('production', function(){
 app.get('/', routes.index);
 app.get('/automata', code.automata);
 app.get('/debug', code.debug);
+app.get('/svg', code.svg);
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/code.js
+++ routes/code.js
@@ -1,8 +1,12 @@
 
 exports.automata = function(req, res){
-  res.render('automata', { title: 'Cellular Automata' })
+    res.render('automata', { title: 'Cellular Automata' })
 };
 
 exports.debug = function(req, res){
-  res.render('debug', { title: 'Debug.js' })
+    res.render('debug', { title: 'Debug.js' })
 };
+
+exports.svg = function(req, res){
+    res.render('svg', { title: 'SVG Playground' })
+}
